45fd70f17c41d0d842d208a47035f0fc01e1c363
Rena <choco.rena0609@gmail.com>
2020-06-07 19:28:15 +0900

    localStorage

--- shopping-cart/pages/index.tsx
+++ shopping-cart/pages/index.tsx
@@ -1,7 +1,27 @@
 import useSWR from "swr";
 import Head from "next/head";
 import Layout from "../component/layout";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+const Counter = ({ itemPrice }: { itemPrice: number }) => {
+  const initialPrice = Number(window.localStorage.getItem("price")) || 0;
+  const [totalPrice, setTotalPrice] = useState<number>(initialPrice);
+
+  useEffect(() => {
+    window.localStorage.setItem("price", String(totalPrice));
+  }, [totalPrice]);
+
+  return (
+    <>
+      <h2>
+        Total ￥<a>{totalPrice}</a>
+      </h2>
+      <button onClick={() => setTotalPrice(itemPrice + totalPrice)}>
+        Add Cart
+      </button>
+    </>
+  );
+};
 
 const getItems = async (query: string) =>
   await fetch("/api/graphql", {
@@ -14,42 +34,37 @@ const getItems = async (query: string) =>
     .then((res) => res.json())
     .then((json) => json.data);
 
-export default function Index() {
+export const Index: React.FC = () => {
   const { data, error } = useSWR(
-    `{items {
-    id
-    name
-    price
-    image
-  }}`,
+    `{items { id, name, price, image }}`,
     getItems
   );
 
-  const [totalPrice, setTotalPrice] = useState<number>(0);
-
   if (error) return <div>Failed to load</div>;
   if (!data) return <div>Loading...</div>;
+
+  if (window.localStorage.getItem("price")) {
+    console.log(window.localStorage.getItem("price"));
+  }
 
   return (
     <Layout>
       <Head>
         <title>shopping cart</title>
       </Head>
-      <h2>
-        Total ￥<a>{totalPrice}</a>
-      </h2>
-      {data?.items.map((item: any) => (
+
+      {data.items.map((item: any) => (
         <div key={item.id}>
           <strong>{item.name}</strong>
           <span> ￥</span>
           <small>{item.price} </small>
-          <button onClick={() => setTotalPrice(item.price + totalPrice)}>
-            Add Cart
-          </button>
+          <Counter itemPrice={item.price} />
           <br />
           <img src={item.image} width="300" height="200" />
         </div>
       ))}
     </Layout>
   );
-}
+};
+
+export default Index;
